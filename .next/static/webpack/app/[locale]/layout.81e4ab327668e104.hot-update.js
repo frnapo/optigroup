399952b762c065737428039cf1a3d6a86fa348ff
Francesco <napo.fra96@gmail.com>
2024-05-08 10:56:43 +0200

    Final merge with testing

---
+++ .next/static/webpack/app/[locale]/layout.81e4ab327668e104.hot-update.js
@@ -0,0 +1,133 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/[locale]/layout",{
+
+/***/ "(app-pages-browser)/./app/[locale]/assets sync recursive ^\\.\\/.*$":
+/*!********************************************!*\
+  !*** ./app/[locale]/assets/ sync ^\.\/.*$ ***!
+  \********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+var map = {
+	"./boy.jpeg": "(app-pages-browser)/./app/[locale]/assets/boy.jpeg",
+	"./dior.jpeg": "(app-pages-browser)/./app/[locale]/assets/dior.jpeg",
+	"./girl.jpeg": "(app-pages-browser)/./app/[locale]/assets/girl.jpeg",
+	"./logo.jpeg": "(app-pages-browser)/./app/[locale]/assets/logo.jpeg",
+	"./logo/logo.png": "(app-pages-browser)/./app/[locale]/assets/logo/logo.png",
+	"./man.jpeg": "(app-pages-browser)/./app/[locale]/assets/man.jpeg",
+	"./most_sold.jpeg": "(app-pages-browser)/./app/[locale]/assets/most_sold.jpeg",
+	"./payments/klarna.png": "(app-pages-browser)/./app/[locale]/assets/payments/klarna.png",
+	"./payments/mastercard.png": "(app-pages-browser)/./app/[locale]/assets/payments/mastercard.png",
+	"./payments/paypal.png": "(app-pages-browser)/./app/[locale]/assets/payments/paypal.png",
+	"./payments/visa.png": "(app-pages-browser)/./app/[locale]/assets/payments/visa.png",
+	"./polarized.jpeg": "(app-pages-browser)/./app/[locale]/assets/polarized.jpeg",
+	"./products/1.png": "(app-pages-browser)/./app/[locale]/assets/products/1.png",
+	"./products/10.png": "(app-pages-browser)/./app/[locale]/assets/products/10.png",
+	"./products/2.png": "(app-pages-browser)/./app/[locale]/assets/products/2.png",
+	"./products/3.png": "(app-pages-browser)/./app/[locale]/assets/products/3.png",
+	"./products/4.png": "(app-pages-browser)/./app/[locale]/assets/products/4.png",
+	"./products/5.png": "(app-pages-browser)/./app/[locale]/assets/products/5.png",
+	"./products/6.png": "(app-pages-browser)/./app/[locale]/assets/products/6.png",
+	"./products/7.png": "(app-pages-browser)/./app/[locale]/assets/products/7.png",
+	"./products/8.png": "(app-pages-browser)/./app/[locale]/assets/products/8.png",
+	"./products/9.png": "(app-pages-browser)/./app/[locale]/assets/products/9.png",
+	"./shape.jpeg": "(app-pages-browser)/./app/[locale]/assets/shape.jpeg",
+	"./style.jpeg": "(app-pages-browser)/./app/[locale]/assets/style.jpeg",
+	"./woman.jpeg": "(app-pages-browser)/./app/[locale]/assets/woman.jpeg"
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "(app-pages-browser)/./app/[locale]/assets sync recursive ^\\.\\/.*$";
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/[locale]/globals.css":
+/*!**********************************!*\
+  !*** ./app/[locale]/globals.css ***!
+  \**********************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (\"e3b5c902cc44\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9bbG9jYWxlXS9nbG9iYWxzLmNzcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsY0FBYztBQUM3QixJQUFJLElBQVUsSUFBSSxpQkFBaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXBwL1tsb2NhbGVdL2dsb2JhbHMuY3NzPzEyYWIiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCJlM2I1YzkwMmNjNDRcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/[locale]/globals.css\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/[locale]/assets/logo/logo.png":
+/*!*******************************************!*\
+  !*** ./app/[locale]/assets/logo/logo.png ***!
+  \*******************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/logo.845c6fef.png\",\"height\":800,\"width\":2584,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.845c6fef.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":2});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvbG9nby9sb2dvLnBuZyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyw2TEFBNkwiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXBwL1tsb2NhbGVdL2Fzc2V0cy9sb2dvL2xvZ28ucG5nP2FhZmUiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL2xvZ28uODQ1YzZmZWYucG5nXCIsXCJoZWlnaHRcIjo4MDAsXCJ3aWR0aFwiOjI1ODQsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGbG9nby44NDVjNmZlZi5wbmcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6Mn07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/[locale]/assets/logo/logo.png\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/[locale]/assets/payments/klarna.png":
+/*!*************************************************!*\
+  !*** ./app/[locale]/assets/payments/klarna.png ***!
+  \*************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/klarna.1a6a04f7.png\",\"height\":176,\"width\":256,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fklarna.1a6a04f7.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":6});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvcGF5bWVudHMva2xhcm5hLnBuZyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxnTUFBZ00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXBwL1tsb2NhbGVdL2Fzc2V0cy9wYXltZW50cy9rbGFybmEucG5nP2VlZmIiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL2tsYXJuYS4xYTZhMDRmNy5wbmdcIixcImhlaWdodFwiOjE3NixcIndpZHRoXCI6MjU2LFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRmtsYXJuYS4xYTZhMDRmNy5wbmcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6Nn07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/[locale]/assets/payments/klarna.png\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/[locale]/assets/payments/mastercard.png":
+/*!*****************************************************!*\
+  !*** ./app/[locale]/assets/payments/mastercard.png ***!
+  \*****************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/mastercard.f80b3ca1.png\",\"height\":176,\"width\":256,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmastercard.f80b3ca1.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":6});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvcGF5bWVudHMvbWFzdGVyY2FyZC5wbmciLCJtYXBwaW5ncyI6IjtBQUFBLCtEQUFlLENBQUMsd01BQXdNIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvcGF5bWVudHMvbWFzdGVyY2FyZC5wbmc/MDQxOSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvbWFzdGVyY2FyZC5mODBiM2NhMS5wbmdcIixcImhlaWdodFwiOjE3NixcIndpZHRoXCI6MjU2LFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRm1hc3RlcmNhcmQuZjgwYjNjYTEucG5nJnc9OCZxPTcwXCIsXCJibHVyV2lkdGhcIjo4LFwiYmx1ckhlaWdodFwiOjZ9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/[locale]/assets/payments/mastercard.png\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/[locale]/assets/payments/paypal.png":
+/*!*************************************************!*\
+  !*** ./app/[locale]/assets/payments/paypal.png ***!
+  \*************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/paypal.5639372e.png\",\"height\":176,\"width\":256,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpaypal.5639372e.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":6});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvcGF5bWVudHMvcGF5cGFsLnBuZyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxnTUFBZ00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vYXBwL1tsb2NhbGVdL2Fzc2V0cy9wYXltZW50cy9wYXlwYWwucG5nPzY3OWIiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL3BheXBhbC41NjM5MzcyZS5wbmdcIixcImhlaWdodFwiOjE3NixcIndpZHRoXCI6MjU2LFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRnBheXBhbC41NjM5MzcyZS5wbmcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6Nn07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/[locale]/assets/payments/paypal.png\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./app/[locale]/assets/payments/visa.png":
+/*!***********************************************!*\
+  !*** ./app/[locale]/assets/payments/visa.png ***!
+  \***********************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/visa.e71bdf69.png\",\"height\":176,\"width\":256,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fvisa.e71bdf69.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":6});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvcGF5bWVudHMvdmlzYS5wbmciLCJtYXBwaW5ncyI6IjtBQUFBLCtEQUFlLENBQUMsNExBQTRMIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8uL2FwcC9bbG9jYWxlXS9hc3NldHMvcGF5bWVudHMvdmlzYS5wbmc/OWJlYSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvdmlzYS5lNzFiZGY2OS5wbmdcIixcImhlaWdodFwiOjE3NixcIndpZHRoXCI6MjU2LFwiYmx1ckRhdGFVUkxcIjpcIi9fbmV4dC9pbWFnZT91cmw9JTJGX25leHQlMkZzdGF0aWMlMkZtZWRpYSUyRnZpc2EuZTcxYmRmNjkucG5nJnc9OCZxPTcwXCIsXCJibHVyV2lkdGhcIjo4LFwiYmx1ckhlaWdodFwiOjZ9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./app/[locale]/assets/payments/visa.png\n"));
+
+/***/ })
+
+});
